72e219349359853017c3298c02e75408aca70669
gleversonrafael <gleverson_rafael@hotmail.com>
2025-01-20 17:59:12 -0300

    0.201

--- js/sign.js
+++ js/sign.js
@@ -1,1 +1,25 @@
 // variables
+const mForm = document.querySelector("#mForm");
+
+mForm.addEventListener("submit", (ev) => {
+    if(checkFields() == true) {
+        ev.preventDefault();
+        
+
+    } else {
+        ev.preventDefault() ;
+    }
+
+});
+
+
+// functions
+function checkFields() {
+    let fields = {namI: true, emaI: true, pasI: true, useI, sPasI: true}
+    
+    if(mForm.name.length = 0) {
+        console.log("Error")
+        return false
+
+    }
+}
