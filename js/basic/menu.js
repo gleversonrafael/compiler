fa00f4cda62a3f7b9049bc9980af3b1d9270cc7a
gleversonrafael <gleverson_rafael@hotmail.com>
2025-01-29 17:28:44 -0300

    sign-out bug fix attempt

--- js/basic/menu.js
+++ js/basic/menu.js
@@ -2,9 +2,48 @@ import { signOut } from "firebase/auth"
 import { auth } from "./fb.js"
 
 
+// var
 let signoutB = document.querySelector("button#signoutB");
+const mIcon = document.getElementById("mIcon");
 
+
+
+
+// events
+mIcon.addEventListener("click", toggleMenu);
+leaveM.addEventListener("click", toggleMenu);
+document.body.addEventListener("resize", toggleMenu);
 
 signoutB.addEventListener("click", () => {
      signOut(auth);
 })
+
+
+
+
+// functions
+function toggleMenu() {
+     console.log("menu toggle")
+     let mMenuBox = document.getElementById("mMenuB");
+
+     if((window.innerWidth >= 768) || (window.innerWidth < 768)) {
+          changeMenuDisplay()   
+     }
+      
+
+     // compl
+     function changeMenuDisplay() {
+          // Menu is not open
+          if(mMenuBox.style.display != "flex") {
+               mMenuBox.style.display = "flex";
+               mIcon.style.display = "none";
+          
+          } else if(window.innerWidth < 768) {
+               mMenuBox.style.display = "none";
+               mIcon.style.display = "block";
+          }
+     }
+}
+
+
+
--- js/final/dashbpf.js
+++ js/final/dashbpf.js
@@ -14,7 +14,7 @@
   \***************************/
 /***/ (() => {
 
-eval("// var\r\nconst mIcon = document.getElementById(\"mIcon\");\r\n\r\n\r\n\r\n\r\nmIcon.addEventListener(\"click\", toggleMenu);\r\nleaveM.addEventListener(\"click\", toggleMenu);\r\n\r\n\r\n\r\n\r\n// functions\r\nfunction toggleMenu() {\r\n     let mMenuBox = document.getElementById(\"mMenuB\");\r\n\r\n     // if(window.innerWidth < 768) {\r\n          if(mMenuBox.style.display == \"flex\") {\r\n               mMenuBox.style.display = \"none\";\r\n               mIcon.style.display = \"block\";\r\n               leaveM.style.display = \"flex\";\r\n     \r\n          } else {\r\n               mMenuBox.style.display = \"flex\";\r\n               mIcon.style.display = \"none\";\r\n          }\r\n\r\n     // }\r\n\r\n}\n\n//# sourceURL=webpack://compiler/./js/basic/dashb.js?");
+eval("\n\n//# sourceURL=webpack://compiler/./js/basic/dashb.js?");
 
 /***/ }),
 
@@ -25,7 +25,7 @@ eval("// var\r\nconst mIcon = document.getElementById(\"mIcon\");\r\n\r\n\r\n\r\
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var firebase_auth__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! firebase/auth */ \"./node_modules/firebase/auth/dist/esm/index.esm.js\");\n/* harmony import */ var _fb_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./fb.js */ \"./js/basic/fb.js\");\n\r\n\r\n\r\n\r\nlet signoutB = document.querySelector(\"button#signoutB\");\r\n\r\n\r\nsignoutB.addEventListener(\"click\", () => {\r\n     (0,firebase_auth__WEBPACK_IMPORTED_MODULE_0__.signOut)(_fb_js__WEBPACK_IMPORTED_MODULE_1__.auth);\r\n})\n\n//# sourceURL=webpack://compiler/./js/basic/menu.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var firebase_auth__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! firebase/auth */ \"./node_modules/firebase/auth/dist/esm/index.esm.js\");\n/* harmony import */ var _fb_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./fb.js */ \"./js/basic/fb.js\");\n\r\n\r\n\r\n\r\n// var\r\nlet signoutB = document.querySelector(\"button#signoutB\");\r\nconst mIcon = document.getElementById(\"mIcon\");\r\n\r\n\r\n\r\n\r\n// events\r\nmIcon.addEventListener(\"click\", toggleMenu);\r\nleaveM.addEventListener(\"click\", toggleMenu);\r\ndocument.body.addEventListener(\"resize\", toggleMenu);\r\n\r\nsignoutB.addEventListener(\"click\", () => {\r\n     (0,firebase_auth__WEBPACK_IMPORTED_MODULE_0__.signOut)(_fb_js__WEBPACK_IMPORTED_MODULE_1__.auth);\r\n})\r\n\r\n\r\n\r\n\r\n// functions\r\nfunction toggleMenu() {\r\n     console.log(\"menu toggle\")\r\n     let mMenuBox = document.getElementById(\"mMenuB\");\r\n\r\n     if((window.innerWidth >= 768) || (window.innerWidth < 768)) {\r\n          changeMenuDisplay()   \r\n     }\r\n      \r\n\r\n     // compl\r\n     function changeMenuDisplay() {\r\n          // Menu is not open\r\n          if(mMenuBox.style.display != \"flex\") {\r\n               mMenuBox.style.display = \"flex\";\r\n               mIcon.style.display = \"none\";\r\n          \r\n          } else if(window.innerWidth < 768) {\r\n               mMenuBox.style.display = \"none\";\r\n               mIcon.style.display = \"block\";\r\n          }\r\n     }\r\n}\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://compiler/./js/basic/menu.js?");
 
 /***/ })
 
