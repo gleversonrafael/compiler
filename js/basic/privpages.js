8f409b7770321fd4e3542848cbbe595f72252361
gleversonrafael <gleverson_rafael@hotmail.com>
2025-01-31 08:10:25 -0300

    3101 bug attempt nOne

--- js/basic/privpages.js
+++ js/basic/privpages.js
@@ -1,6 +1,8 @@
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./fb.js"
 
+console.log(auth);
+
 
 // redirect to the first page if there is no user
 onAuthStateChanged(auth, (userData) => {
@@ -13,11 +15,10 @@ onAuthStateChanged(auth, (userData) => {
 // // functions
 function analyzeLogState(data) {
      // user is not logged in, therefore he doesn't have acess to the site 
-     if(data === null && !window.location.href.includes("index") && !window.location.href.includes("log") && !window.location.href.includes("compiler")) {
+     if(data === null && !window.location.href.includes("index") && !window.location.href.includes("log") && window.location.href != "https://gleversonrafael.github.io/compiler/" ) {
           // GITHUB COMPATIBILITY
           if(window.location.href.includes("github")) {
-               // window.location.href = "https://gleversonrafael.github.io/compiler/"
-               console.log("test");
+               window.location.href = "https://gleversonrafael.github.io/compiler/"
 
           } else {
                window.location.href = "./../index.html";
@@ -27,19 +28,27 @@ function analyzeLogState(data) {
 
 
      // user is logged in and tries to acess start page / login (GITHUB COMPATIBILITY)
-     if(data != null && ((window.location.href.includes("index")) || (window.location.href.includes("compiler")))) {
-          // window.location.href = "./html/dashboard.html";  
-          console.log("test2");
-     }
+     // if(data != null && ( window.location.href.includes("index") || window.location.href == "https://gleversonrafael.github.io/compiler/" || window.location.href.includes("log"))) {
+     //      if(window.location.href.includes("github")) {
+     //           window.location.href = "https://gleversonrafael.github.io/compiler/html/dashboard.html"
+
+     //      } else{
+     //           // window.location.href = window.location.href.includes("login") ? "./html/dashboard.html" : "./dashboard.html"
+     //           console.log("Redirecionado")
+     //           console.log(data);
+     //      }
+     
+     // }
 
      
-     if(data != null && window.location.href.includes("log")) {
-          let passInp = document.getElementById("passInp");
+     // if(data != null && window.location.href.includes("log")) {
+     //      let passInp = document.getElementById("passInp");
 
-          // is the password input filled? prevents user from being auto redirected afther finishing the form.
-          if(passInp.value.length == 0) {
-               window.location.href = "./dashboard.html"
-          }
 
-     }  
+     //      // is the password input filled? prevents user from being auto redirected afther finishing the form.
+     //      if(passInp.value.length == 0) {
+     //           window.location.href = "./dashboard.html"
+     //      }
+
+     // }  
 }
