e582a97752d1b06de691ac8189049c53cdbae6c1
gleversonrafael <gleverson_rafael@hotmail.com>
2025-02-06 18:58:53 -0300

    0602

--- js/basic/mycourses.js
+++ js/basic/mycourses.js
@@ -7,46 +7,158 @@ import { db } from "./fb.js";
 // var
 const coursesCol = collection(db, "courses");
 let coursesData = {};
+let searchButton = document.getElementById("searchButton");
+let searchInp = document.getElementById("searchInp");
 
 
-obtainAllowedCoursesData();
-createCoursesBoxes("Teste", "Plataforma")
 
 
-function showCourses() {
-     if(obtainAllowedCoursesData() === true) {
+// events
+// document.body.addEventListener("load", showCourses);
+searchButton.addEventListener("click", () => {
+     showCourses(searchInp.value);
+});
+
+searchInp.addEventListener("input", () => {
+     showCourses(searchInp.value);
+})
+
+
+
+
+// functions
+// main level
+async function showCourses(searchedContent) {
+     if(await obtainAllowedCoursesData(searchedContent) === true) {
           createCoursesBoxes();
+     
+     } else {
+          let coursesA = document.getElementById("coursesA");
+
+          coursesA.innerHTML = "";
+          // noDataObtained();
+
      }
 
 }
 
 
-async function obtainAllowedCoursesData() {
-     // var
-     let mycoursesQuery = query(coursesCol, where("usersWithAcess", "array-contains", userData.uid));
+function openBox(event) {  
+     let courseId = obtainClickedCourseId();
+     let elementData = coursesData[courseId];
+     
+     openBox();
+
+
+
+     // compl    
+     function obtainClickedCourseId() {
+          let clickedElement = event.target;
+
+          // div child clicked?
+          if(clickedElement.childElementCount == 0) {
+               return clickedElement.parentElement.id;
+          
+          } else {
+               return clickedElement.id;
+          }
+     }
+
+
+     function openBox() {
+          let courseBox = document.getElementById(courseId);
+          courseBox.classList.add("open");
+
+          createElements()
+
+
+
+          // aside
+          function createElements() {
+               // var
+               let elements = {
+                    divType: {
+                         createdContent: document.createElement("div"),
+
+                         emailDiv: document.createElement("div"),
+                         emailDivChild: document.createElement("div"),
+
+                         passwordDiv: document.createElement("div")
+                    },
+
+
+                    pType: {
+                         emailP: document.createElement("p"),
+                         emailValue: document.createElement("p"),
+
+                         passwordP: document.createElement("p"),
+                         passwordValue: document.createElement("p")
+                    },
+
+
+                    others: {
+                         userDataSubtitle: document.createElement("h3"),
+                         acessAnchor: document.createElement("a"),
+
+                         copyEmail: document.createElement("button"),
+                         copyPassword: document.createElement("button")
+                    }
+               }
+
+
+
+               // p
+               elements.pType.emailP.innerText = "E-mail";
+               elements.pType.emailValue.innerText = elementData[email];
+
+               elements.pType.passwordP.innerText = "Senha";
+               elements.pType.passwordValue.innerText = elementData[userPassword];
+
+               // others
+               elements.others.userDataSubtitle.innerText = "Dados";
+
+
+               // divType 
+               // elements.divType.createdContent.appendChild(elements.others.userDataSubtitle)
+               // elements.divType.createdContent.appendChild
+
+               // elements.divType.createdContent.append(elements.others.userDataSubtitle, elements.others)
+
+               console.log(elements);
+
+          }
+     }
+}
+
+
+
+
+// secondary level
+async function obtainAllowedCoursesData(searchedContent) {
+     // query process
+     let mycoursesQuery = createQuery();
+
 
      let insertData = new Promise((dataObtained, noData) => {
           onSnapshot(mycoursesQuery, (dataState) => {
                // add value to coursesData
                dataState.forEach((currentDocument)=> {
                     Object.defineProperty(coursesData, currentDocument.id, {
-                         value: currentDocument.data()
+                         value: currentDocument.data(),
+                         writable: true,
+                         configurable: true,
+                         enumerable: true
      
                     })
-     
                })
 
 
                // is there any data obtained?
-               console.log(Object.entries(coursesData));
-          
-
-               if(Object.keys(coursesData).length === 0) {
-                    noData("Array vazio!");     
+               if(Object.entries(coursesData).length === 0) {
+                    noData("No courses were found.");     
                
                } else {
-                    dataObtained("Array com elementos em seu interior!");
-                    
+                    dataObtained();   
                }
           })
 
@@ -54,43 +166,84 @@ async function obtainAllowedCoursesData() {
 
 
 
-     // m process
-     await insertData
-     .then((msg) => {
-          console.log(msg);
-          console.log("Sucesso!");
+     // process
+     let insertDataResult;
+     coursesData = {};
 
-          return true
+
+     await insertData
+     .then(() => {
+          insertDataResult = true
 
      })
      .catch((msg) => {
           console.log(msg);
-          console.log("There is no courses assigned.");
-
-          return false
+          insertDataResult = false
      })
+
+
+     return insertDataResult
+
+
+
+     // complementary
+     function createQuery() {
+          let queryResult;
+
+
+          if(searchedContent === undefined || searchedContent === "") {
+               queryResult = query(coursesCol, where("usersWithAcess", "array-contains", userData.uid));
+          
+          } else {
+               queryResult = query(coursesCol, where("usersWithAcess", "array-contains", userData.uid), where("courseName", "==",  searchedContent));  
+
+          }
+
+          return queryResult
+     }
+
 
 }
 
 
 
-function createCoursesBoxes(name, email) {
-     console.log("Fired");
-     let coursesA = document.getElementById("coursesA");
-     console.log(coursesA);
 
-     let div = document.createElement("div");
-     let h1 = document.createElement("h1");
-     h1.textContent = name
-
-     let h2 = document.createElement("h2");     
-     h2.textContent = email
-
-     div.appendChild(h1);     
-     div.appendChild(h2);
-
-     console.log(div);
+// third level
+function createCoursesBoxes() {
+     let coursesArea =  document.querySelector("section#coursesA")
+     coursesArea.innerHTML = "";
 
 
-     coursesA.appendChild(div)
+     Object.entries(coursesData).forEach((data) => {
+          // var
+          let courseId = data[0];
+          let courseValues = data[1];
+
+
+          let courseProperties = {
+               courseBox: document.createElement("div"), 
+               title: document.createElement("h1"),
+               platform: document.createElement("h2")
+          }
+
+
+
+          // ids and event
+          courseProperties.courseBox.id = courseId;
+          courseProperties.courseBox.onclick = openBox;
+
+
+          // set data
+          courseProperties.title.innerText = courseValues.courseName;
+          courseProperties.platform.innerText = `Plataforma:${courseValues.coursePlatform}`;
+
+
+          // remover nomes e subtitulos no objeto posteriormente, para agilizar o programa
+
+          // create elements
+          courseProperties.courseBox.appendChild(courseProperties.title);
+          courseProperties.courseBox.appendChild(courseProperties.platform);
+          coursesArea.appendChild(courseProperties.courseBox);
+     })
+
 }
