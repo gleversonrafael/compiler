36bb833aac4174e585cdbfc998a103fd43318d94
gleversonrafael <gleverson_rafael@hotmail.com>
2025-03-14 15:40:48 -0300

    firebase admin sdk - resolvepath001

--- js/adminfb.js
+++ js/adminfb.js
@@ -1,3 +1,4 @@
 import { initializeApp } from "firebase-admin";
+console.log(initializeApp);
 
 // Continuar no futuro - server-side (usuários )
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+// client tecnologies
 module.exports = {
   mode: "development",
 
@@ -16,8 +17,6 @@ module.exports = {
       import : ["./js/basic/general/jsmenu.js", "./js/basic/jspagechange.js", "./js/basic/general/jsreusablestructures.js"]
     },
 
-    
-
     mycourses: {
       import: ["./js/basic/managemycourses/jsmanageandmycourses.js", "./js/basic/managemycourses/jsmycourses.js"]
 
@@ -28,7 +27,7 @@ module.exports = {
     },
 
     manageusers: { 
-      import: ["./js/basic/jsmanageusers.js", "./js/adminfb.js"] 
+      import: ["./js/adminfb.js", "./js/basic/jsmanageusers.js"] 
     }, 
 
     myuser: { 
@@ -41,4 +40,92 @@ module.exports = {
     path: path.resolve(__dirname, "./js/final"),
     filename: '[name]bundle.js',
   },
+
+
+  resolve: {
+    fallback: {
+      fs: false, 
+      
+      // firebase sdk
+      util: false,
+      stream: false,
+      events: false,
+      process: false,
+
+      net: false,
+      tls: false,
+      http2: false,
+      child_process: false,
+
+
+      // others
+      assert: false,
+      buffer: false,
+      console: false,
+      constants: false,
+      crypto: false,
+      domain: false,
+      http: false,
+      https: false,
+      os: false,
+      path: false,
+      punycode: false,
+      querystring: false,
+      string_decoder: false,
+      sys: false,
+      timers: false,
+      tty: false,
+      url: false,
+      vm: false,
+      zlib: false,
+    }
+
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// other way of doing -- target web for serveside
+// // server tecnologies
+// const serverSide = {
+//   mode: "development",
+//   target: "node",
+
+//   entry: {
+//     manageusers: {
+//       import: ["./js/adminfb.js"]
+
+//     }
+
+//   },
+
+//   output: {
+//     path: path.resolve(__dirname, "./js/final"),
+//     filename: "[name]serverside.js"
+  
+//   },
+
+//   resolve: {
+//     fallback: {
+//       fs: false,
+//       path: false
+
+//     }
+
+//   }
+// }
