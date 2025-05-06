ab8a82e981051d03ad54f37436ea8566b0c69b62
gleversonrafael <gleverson_rafael@hotmail.com>
2025-05-06 17:28:05 -0300

    launch

--- js/basic/jsmain.js
+++
@@ -1,8 +0,0 @@
-import { setFunctionsOnLoad, deletePlatformLoad } from "./general/jsload.js"
-import { changePage } from "../basic/general/jspagechange.js"
-
-setFunctionsOnLoad([deletePlatformLoad]);
-await changePage(
-    window.location.search.replace("?currentpage=", ""), 
-    `../html/${window.location.search.replace("?currentpage=", "")}.html`
-);
--- js/basic/managemycourses/jsmycourses.js
+++
@@ -1,6 +0,0 @@
-function copyData(ev) {
-     let fieldValue = ev.currentTarget.parentElement.children[1].innerText;
-     navigator.clipboard.writeText(fieldValue);
-}
-
-export { copyData }
